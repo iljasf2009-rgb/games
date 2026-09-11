758aca7879fe675e674242d1e0ed681c97fe2784
Ilyas Farah <iljas.f2009@gmail.com>
2026-09-11 11:24:26 +0200

    Create: formulier en store .

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Game;
+use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
@@ -10,5 +11,31 @@ class GameController extends Controller
 {
     $games = Game::all();
     return view('games.index', compact('games'));
+}
+
+public function create()
+{
+    return view('games.create');
+}
+
+public function store(Request $request)
+{
+    $request->validate([
+        'game_name' => 'required',
+        'platform' => 'required',
+        'genre' => 'required',
+        'rating' => 'required|numeric|min:0|max:10'
+    ]);
+
+    $game = new Game([
+        'game_name' => $request->get('game_name'),
+        'platform' => $request->get('platform'),
+        'genre' => $request->get('genre'),
+        'rating' => $request->get('rating')
+    ]);
+
+    $game->save();
+
+    return redirect('/games')->with('success', 'Game added!');
 }
 }
