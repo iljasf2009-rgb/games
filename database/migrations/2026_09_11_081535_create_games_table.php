7425f2176938890d778ad215ac442b4baa971a95
Ilyas Farah <iljas.f2009@gmail.com>
2026-09-11 10:18:59 +0200

    Database migratie en Game model .

---
+++ database/migrations/2026_09_11_081535_create_games_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+public function up()
+{
+    Schema::create('games', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('game_name');
+        $table->string('platform');
+        $table->string('genre');
+        $table->decimal('rating', 3, 1);
+        $table->timestamps();
+    });
+}
+};
