3bcc3fe49cafa07bc017c055ec8bd1f5fde69d73
Ilyas Farah <iljas.f2009@gmail.com>
2026-09-11 10:32:42 +0200

    Read: controller, view en route .

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Game;
 
 class GameController extends Controller
 {
-    //
+    public function index()
+{
+    $games = Game::all();
+    return view('games.index', compact('games'));
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('games', [App\Http\Controllers\GameController::class, 'index']);{
     return view('welcome');
-});
+};
